94016cdc498066a357f813dfae0894482282cd2b
Mateusz Koniarski <mateusz.koniarski@gmail.com>
2016-12-12 12:51:40 +0100

    Unit tests filnemes and assert-subset in progress

---
+++ test/odata-v4-parser-filter.spec.ts
@@ -0,0 +1,12 @@
+// import { expect } from 'chai';
+// import { Parser } from '../src/parser';
+// import { assertIsSubset } from './assert-utils';
+
+// describe('odataV4Parser filter', function () {
+//     it('should propperly parse filter with lower than ..', function () {
+//         var filterQuery = "Categories?$filter=Products/$count lt 10";
+//         var token = new Parser().odataUri(filterQuery);
+//         console.log(JSON.stringify(token));
+//         //expect(token).to.be.deep.equal(expected);
+//     });
+// });
